f6c1c1336b81ef645c9a5e5a3bee43e9c6d3d330
Nick Day <thenickday@icloud.com>
2020-01-24 02:12:32 -0500

    heroku

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const helmet = require('helmet')
 const cors = require('cors')
+const helmet = require('helmet')
 const POKEDEX = require('./pokedex.json')
 
 const app = express()
 
 const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common'
 app.use(morgan(morganSetting))
-app.use(helmet())
 app.use(cors())
+app.use(helmet())
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN
